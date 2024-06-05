4ff888d3cc125acdd1e5fd4341a4ceebbd379b35
YAREN BOS <225040093@stu.istinye.edu.tr>
2024-06-05 06:37:32 +0300

    game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,20 +7,37 @@ public class Player : MonoBehaviour
     public float moveSpeed;
     private Rigidbody2D rb;
 
+    public float jumpForce;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayers;
+    private bool isGrounded;
+
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        anim=GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayers);
+
         rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        anim.SetBool("OnGround",isGrounded);
 
     }
 }
